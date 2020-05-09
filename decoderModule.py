2ce8e77df4b13f8dc072ca2c2b65105a0cf62147
Luz05 <62961533+Luz05@users.noreply.github.com>
2020-05-09 02:47:20 -0500

    n ready
    letras y números

--- decoderModule.py
+++ decoderModule.py
@@ -24,9 +24,56 @@ def decodes(code,advancekey):
 
 			index_list = list_alphabet.index(char_to_explore)
 			decode_index = index_list + int(advancekey)
-			FOR 
 			decoded_string = decoded_string + list_alphabet[decode_index]
+		else:
+			index_list = list_number.index(char_to_explore)
+			decode_index = index_list + int(advancekey)
+			decoded_string = decoded_string + list_number[decode_index]
+		length_iterator = length_iterator + 1
+
+	print ("decoded chain: " + decoded_string)
+
+def main():
+	
+	print ("-------- RebelAllianceDecoderModule ---------")
+	
+	inputx = input ("R2 insert the key to decode: ")
+	input_advance = input ("R2 insert the advance key to generate the decode result: ")
+	decodes(inputx,input_advance)
+
+if __name__ == "__main__":
+	main()
+
+def split(word): 
+    return [char for char in word]  
+
+def decodes(code,advancekey):
+
+	length_iterator = 0 
+	decoded_string = ""
+	code_upper = code.upper()
+	decoded_list = split(code_upper)
+
+	list_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", 
+	"J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+	
+	list_number = ["0", "1" , "2", "3", "4", "5", "6", "7", "8", "9"]
+	
+	print ("original chain: " + code_upper)
+	
+	while length_iterator < len(decoded_list):
 		
+		char_to_explore = decoded_list[length_iterator]
+
+		if char_to_explore.isalpha():
+
+			index_list = list_alphabet.index(char_to_explore)
+			decode_index = index_list + int(advancekey)
+			decoded_string = decoded_string + list_alphabet[decode_index]
+		else:
+			index_list = list_number.index(char_to_explore)
+			decode_index = index_list + int(advancekey)
+			decoded_string = decoded_string + list_number[decode_index]
 		length_iterator = length_iterator + 1
 
 	print ("decoded chain: " + decoded_string)
